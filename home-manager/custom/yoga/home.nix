cbf591bf20c1dddc612b53d2f6ffbaf7cea5715e
Ola Berglund <olakjberglund@gmail.com>
2025-01-03 15:29:11 +0100

    hm stub for cursor change

--- home-manager/custom/yoga/home.nix
+++ home-manager/custom/yoga/home.nix
@@ -42,4 +42,23 @@
       ''}";
     };
   };
+  # home.pointerCursor = let
+  #   getFrom = url: hash: name: {
+  #     gtk.enable = true;
+  #     x11.enable = true;
+  #     name = name;
+  #     size = 48;
+  #     package = pkgs.runCommand "moveUp" { } ''
+  #       mkdir -p $out/share/icons
+  #       ln -s ${
+  #         pkgs.fetchzip {
+  #           url = url;
+  #           hash = hash;
+  #         }
+  #       } $out/share/icons/${name}
+  #     '';
+  #   };
+  # in getFrom
+  # "https://github.com/ful1e5/fuchsia-cursor/releases/download/v2.0.0/Fuchsia-Pop.tar.gz"
+  # "sha256-BvVE9qupMjw7JRqFUj1J0a4ys6kc9fOLBPx2bGaapTk=" "Fuchsia-Pop";
 }
