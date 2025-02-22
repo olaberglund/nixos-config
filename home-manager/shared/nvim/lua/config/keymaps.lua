0917632739c1146fad51202565e9a925ff4e6fee
Ola Berglund <olakjberglund@gmail.com>
2025-02-22 18:14:58 +0100

    upgrade lazy vim

--- home-manager/shared/nvim/lua/config/keymaps.lua
+++ home-manager/shared/nvim/lua/config/keymaps.lua
@@ -1,4 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<C-e>", "<cmd>Neotree focus<cr>", { desc = "NeoTree focus" })
--- home-manager/shared/nvim/lua/plugins/indent-blankline.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  tag = "v3.5.4",
-}
